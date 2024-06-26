6b65a4ba8d3036d5ed1335539af2549ab4e850e9
Samrat Thapa <38401989+SamratThapa120@users.noreply.github.com>
2024-06-26 10:47:33 +0900

    added support for autoware_perception_msgs (#1)

--- src/index.ts
+++ src/index.ts
@@ -216,15 +216,30 @@ export function activate(extensionContext: ExtensionContext): void {
     toSchemaName: "foxglove.SceneUpdate",
     converter: convertPredictedObjects,
   });
+  extensionContext.registerMessageConverter({
+    fromSchemaName: "autoware_perception_msgs/msg/PredictedObjects",
+    toSchemaName: "foxglove.SceneUpdate",
+    converter: convertPredictedObjects,
+  });
 
   extensionContext.registerMessageConverter({
     fromSchemaName: "autoware_auto_perception_msgs/msg/TrackedObjects",
     toSchemaName: "foxglove.SceneUpdate",
     converter: convertTrackedObjects,
   });
+  extensionContext.registerMessageConverter({
+    fromSchemaName: "autoware_perception_msgs/msg/TrackedObjects",
+    toSchemaName: "foxglove.SceneUpdate",
+    converter: convertTrackedObjects,
+  });
 
   extensionContext.registerMessageConverter({
     fromSchemaName: "autoware_auto_perception_msgs/msg/DetectedObjects",
+    toSchemaName: "foxglove.SceneUpdate",
+    converter: convertDetectedObjects,
+  });
+  extensionContext.registerMessageConverter({
+    fromSchemaName: "autoware_perception_msgs/msg/DetectedObjects",
     toSchemaName: "foxglove.SceneUpdate",
     converter: convertDetectedObjects,
   });
